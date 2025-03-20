aed473902de400b3fb31a01a1e100717fa5baa97
abrakablam <165574894+abrakablam@users.noreply.github.com>
2025-03-19 21:17:03 -0400

    Added line, moved graphics to new file.

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window:
-    def __init__(self, width, height) -> None:
-        self.__root = Tk()
-        self.__root.title("Mazerunner")
-        self.__canvas = Canvas(self.__root, bg="dim grey", height=height, width=width)
-        self.__canvas.pack(fill=BOTH, expand=1)
-        self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.__running = True
-        
-        while self.__running == True:
-            self.redraw()
-        print("Window closed. Goodbye.")
-    def close(self):
-        self.__running = False
-
+from graphics import Window, Line, Point 
 
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "red")
+
     win.wait_for_close()
+
 
 if __name__ == "__main__":
     main()
