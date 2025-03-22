0ed65a3e41ad11649d4db74b9ab7cfe95049c58e
abrakablam <165574894+abrakablam@users.noreply.github.com>
2025-03-22 18:22:36 -0400

    Finalized maze runner. Will want to rebuild from scratch.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from graphics import Window
 from maze import Maze
-
+import sys
 
 def main():
     num_rows = 12
@@ -10,9 +10,19 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_columns
     cell_size_y = (screen_y - 2 * margin) / num_rows
+
+    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_columns, cell_size_x, cell_size_y, win)
+    maze = Maze(margin, margin, num_rows, num_columns, cell_size_x, cell_size_y, win, 10)
+    print("Maze created")
+
+    is_solvable = maze.solve()
+
+    if not is_solvable:
+        print("The maze cannot be solved, whoops.")
+    else:
+        print("Maze has been solved!")
     win.wait_for_close()
 
 
